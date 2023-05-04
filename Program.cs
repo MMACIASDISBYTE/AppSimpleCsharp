be3252a38b6d8143a45e361a72242cb158b2a8fc
mmaciasdisbyte <mmacias@disbyte.com>
2023-05-04 10:48:13 -0300

    cambiamos una linea para probar el pull

--- Program.cs
+++ Program.cs
@@ -3,19 +3,21 @@ Console.WriteLine("Rock Paper Scissors");
 
 
 
-while(true){
-    Console.WriteLine("Are you ready?");
-    Console.WriteLine("Let's begin!!!");
+// while(true){
+//     Console.WriteLine("Are you ready?");
+//     Console.WriteLine("Let's begin!!!");
 
-    Console.WriteLine("Choose R, P or S []: [R]ock, [P]aper, or [S]cissors: ");
-    var selectedChoise = Console.ReadLine(); //espera a q el usuario escriba un comando
+//     Console.WriteLine("Choose R, P or S []: [R]ock, [P]aper, or [S]cissors: ");
+//     var selectedChoise = Console.ReadLine(); //espera a q el usuario escriba un comando
 
-    // if(selectedChoise?.ToLower() != 'r' or )
-    Console.WriteLine(selectedChoise);
+//     // if(selectedChoise?.ToLower() != 'r' or )
+//     Console.WriteLine(selectedChoise);
 
-    if(selectedChoise!=null && selectedChoise.Contains("R"))
-    {
-        Console.WriteLine("Rocks");
-    }
+//     if(selectedChoise!=null && selectedChoise.Contains("R"))
+//     {
+//         Console.WriteLine("Rocks");
+//     }
     
-}; 
+// }; 
+
+Console.WriteLine('Hola Peter probando tu primer pull');
