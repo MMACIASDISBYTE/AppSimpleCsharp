350fea51432f4dc003dfd6d04a898ee9f1231191
mmaciasdisbyte <mmacias@disbyte.com>
2023-05-04 10:06:04 -0300

    mi primer commit con CSharp

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Rock Paper Scissors");
+
+while(true){
+    Console.WriteLine("Are you ready?");
+    Console.WriteLine("Let's begin!!!");
+
+    Console.WriteLine("Choose R, P or S []: [R]ock, [P]aper, or [S]cissors: ");
+    var selectedChoise = Console.ReadLine(); //espera a q el usuario escriba un comando
+
+    // if(selectedChoise?.ToLower() != 'r' or )
+    Console.WriteLine(selectedChoise);
+}; 
